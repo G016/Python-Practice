27ec8ecdc6baca86bcd89253e2b82aed0fb2d7ae
G016 <g.pverma716@gmail.com>
2023-02-13 16:53:29 +0530

    G016.Gaurav Verma

---
+++ Inheritance.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, fname, mname, lname):
+    self.firstname = fname
+    self.middlename = mname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.middlename, self.lastname)
+
+x = Person("Gaurav", "Kumar", "Verma")
+x.printname()
