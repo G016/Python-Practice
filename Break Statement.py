aba10b76bf5c556fb782652f08e02c1cd128ea8e
G016 <g.pverma716@gmail.com>
2023-03-05 23:47:15 +0530

    New

---
+++ Break Statement.py
@@ -0,0 +1,6 @@
+Totals = [44,76,98,45,34,66,75,23,24,66,12,60,96]
+
+for Marks in Totals:
+    if Marks == 24:
+        break    
+    print (Marks)
---
+++ Continue Statement.py
@@ -0,0 +1,6 @@
+students = ["Ram","Shyam","Sameer","Manoj","Kishan","Akhlesh","Gaurav"]
+
+for person in students:
+    if person == "Sameer":
+        continue
+    print (person)
